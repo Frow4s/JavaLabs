cc9f59acc3d6f179780e634c6aab86d66b4f223b
nikisim <nikisim2000@gmail.com>
2019-03-15 20:09:41 +0300

    Теперь измененный файл тоже называется input.xml

--- src/To_xml_file.java
+++ src/To_xml_file.java
@@ -6,13 +6,24 @@ import java.util.Scanner;
 public class To_xml_file {
 
     public static void to_xml(Gryadka gryadka) throws FileNotFoundException{
-        PrintWriter pw = new PrintWriter("src/input1.xml"); //я хочу еще сделать так,
-        File file = new File("src/input.xml");  // чтобы эти файлы были под одним именем
+
+        String[] lines = new String[20];
+
+        File file = new File("src/input.xml");
         Scanner scan = new Scanner(file);
+        int i = 0;
         while (scan.hasNext()){
             String line = scan.nextLine();
-            pw.println(line);
+            lines[i] = line;
+            i++;
         }
+
+        PrintWriter pw = new PrintWriter("src/input.xml");
+        for (String str : lines){
+            if (str != null)
+                pw.println(str);
+        }
+
 
         String intro = "<gryadkas>";
         String name = gryadka.getType();
